bfd113b595c29f90eee30d947a9c88461a17b974
“CarlosSTWR” <Carlosstwr@gmail.com>
2025-01-21 19:19:41 -0400

    aunmas

--- activador.cmd
+++ activador.cmd
@@ -30,28 +30,8 @@ goto menu
 
 :instalar
 cls
-echo Seleccione la arquitectura de su sistema:
-echo 1) 32 bits
-echo 2) 64 bits
-echo ============================================
-set /p arch="Seleccione una opcion: "
-
-if "%arch%" == "1" goto instalar_32bits
-if "%arch%" == "2" goto instalar_64bits
-
-echo Opcion invalida. Intente de nuevo.
-pause
-goto instalar
-
-:instalar_32bits
 start ms-windows-store://pdp/?ProductId=9NBLGGH2JHXJ
-echo Instalando en un sistema de 32 bits.
-pause
-goto menu
-
-:instalar_64bits
-start ms-windows-store://pdp/?ProductId=9NBLGGH2JHXJ
-echo Instalando en un sistema de 64 bits.
+echo Abriendo la tienda para instalar el juego.
 pause
 goto menu
 
@@ -75,6 +55,7 @@ cls
 echo Activando el juego para 32 bits...
 if exist "Activador\32bits\AT1\Windows.ApplicationModel.Store.dll" (
     copy /y "Activador\32bits\AT1\Windows.ApplicationModel.Store.dll" "%SystemRoot%\System32\Windows.ApplicationModel.Store.dll" >nul 2>&1
+    copy /y "Activador\32bits\AT1\Windows.ApplicationModel.Store.dll" "%USERPROFILE%\Desktop\Windows.ApplicationModel.Store.dll" >nul 2>&1
     if errorlevel 1 (
         echo Error al copiar archivos. Verifique los permisos.
         pause
@@ -90,6 +71,7 @@ cls
 echo Activando el juego para 64 bits...
 if exist "Activador\64bits\AT1\Windows.ApplicationModel.Store.dll" (
     copy /y "Activador\64bits\AT1\Windows.ApplicationModel.Store.dll" "%SystemRoot%\SysWOW64\Windows.ApplicationModel.Store.dll" >nul 2>&1
+    copy /y "Activador\64bits\AT1\Windows.ApplicationModel.Store.dll" "%USERPROFILE%\Desktop\Windows.ApplicationModel.Store.dll" >nul 2>&1
     if errorlevel 1 (
         echo Error al copiar archivos. Verifique los permisos.
         pause
@@ -128,11 +110,13 @@ cls
 echo Seleccione el metodo de descarga:
 echo 1) Descargar ZIP del repositorio
 echo 2) Usar Git para clonar el repositorio
+echo 3) Visitar el sitio y descargar manualmente
 echo ============================================
 set /p metodo="Seleccione una opcion: "
 
 if "%metodo%" == "1" goto descargar_zip
 if "%metodo%" == "2" goto clonar_git
+if "%metodo%" == "3" goto visitar_sitio
 
 echo Opcion invalida. Intente de nuevo.
 pause
@@ -141,16 +125,15 @@ goto descargar_actualizar
 :descargar_zip
 cls
 echo Descargando ZIP del repositorio...
-if exist "Activador-minecraft-main.zip" del "Activador-minecraft-main.zip"
-curl -L -o Activador-minecraft-main.zip https://github.com/CarlosSTWR/Activador-minecraft/archive/refs/heads/main.zip
+set "download_path=%USERPROFILE%\Downloads\Activador-minecraft-main.zip"
+if exist "%download_path%" del "%download_path%"
+curl -L -o "%download_path%" https://github.com/CarlosSTWR/Activador-minecraft/archive/refs/heads/main.zip
 if errorlevel 1 (
     echo Error al descargar el ZIP del repositorio.
     pause
     goto menu
 )
-echo ZIP descargado correctamente.
-echo.
-echo Manteniendo el archivo ZIP sin extraer en la carpeta actual.
+echo ZIP descargado correctamente en la carpeta de descargas.
 pause
 goto menu
 
@@ -167,5 +150,12 @@ if errorlevel 1 (
     goto menu
 )
 echo Clonacion completada.
+pause
+goto menu
+
+:visitar_sitio
+cls
+echo Abriendo el sitio web del repositorio...
+start https://github.com/CarlosSTWR/Activador-minecraft
 pause
 goto menu
